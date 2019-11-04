856834fe7c47890770c57a23268812325cbf349d
unknown <52844667+a4444304@users.noreply.github.com>
2019-11-03 20:36:20 -0600

    initial setup

---
+++ Traversy Media/codegig_db.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS codegig_db;
+
+
+CREATE DATABASE codegig_db;
+
+USE codegig_db;
+
+
+CREATE TABLE gigs(
+  id int NOT NULL AUTO_INCREMENT,
+  title VARCHAR(255) NOT NULL,
+  technologies VARCHAR(255) NOT NULL,
+  budget VARCHAR(20) NOT NULL,
+  description TEXT NOT NULL,
+  contact_email VARCHAR(255) NOT NULL,
+  createdAt DATE NOT NULL,
+  updatedAt DATE NOT NULL,
+  PRIMARY KEY (id)
+);
+
+-- Insert a set of records.
+INSERT INTO movies (movie) VALUES ('Gone with the Wind');
