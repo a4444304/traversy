febaf6d5e92bd9b2909e2a07feb5b832e1827e93
unknown <52844667+a4444304@users.noreply.github.com>
2019-11-07 03:04:34 -0600

    added edit button

--- app.js
+++ app.js
@@ -29,6 +29,33 @@ app.get("/", (req, res) => res.render("index", { layout: "landing" }));
 // Gig routes
 app.use("/gigs", require("./routes/gigs(routes)"));
 
+// Load Edit Form, Brad I have no idea if this will work. 
+// I am trying to make our edit button do something.
+app.get("/views/add.handlebars", function(req, res){
+    EditJobPost.findByClass(req. params. class, function(err, edit){
+        res.render("edit_jobPost", {
+            title:"edit title",
+            edit:edit
+        });
+    });
+});
+// Update Submit POST Route
+app.post("/views/edit.handlebars", function(req, res){
+    let edit = new EditJobPost();
+    edit.title = req.body.title;
+    edit.technologies = req.body.technologies;
+    edit.budget = req.body.budget;
+
+    edit.save(function(err){
+        if(err){
+            console.log(err);
+            return;
+        } else {
+            res.redirect("/");
+        }
+    });
+});
+// Start Server
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
